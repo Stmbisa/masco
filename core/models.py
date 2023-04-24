6131d4c90604ac15952f7c6f837b6a352f227f39
jc mbisa <jcmbisa@gmail.com>
2023-04-24 04:26:04 +0300

    new models trainer, contacts

--- core/models.py
+++ core/models.py
@@ -88,10 +88,13 @@ class Blog(models.Model):
     def __str__(self):
         return self.title
     
-
-
     
+class Contact(models.Model):
+    name=models.CharField(max_length=25)
+    email=models.EmailField()
+    phonenumber=models.CharField(max_length=12)
+    description=models.TextField()
+
+    def __str__(self):
+        return self.email
     
-
-
-
--- users/models.py
+++ users/models.py
@@ -111,6 +111,20 @@ class User(AbstractBaseUser, PermissionsMixin):
             message = render_to_string('membership_expiry_email.html', {'user': self})
             send_mail(subject, message, 'gym@example.com', [self.email])
 
+    def __str__(self):
+        return self.name
+
+
+class Trainer(models.Model):
+    account = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    name=models.CharField(max_length=55)
+    gender=models.CharField(max_length=25)
+    phone=models.CharField(max_length=25)
+    salary=models.IntegerField()
+    timeStamp=models.DateTimeField(auto_now_add=True,blank=True)
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
     
 
 
